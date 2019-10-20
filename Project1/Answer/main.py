358562d196b68af45ca27a90bb4da67c52797d7c
Vin-itall <vinitacharya8@gmail.com>
2019-10-20 09:03:55 +0530

    Cleaning Up

--- Project1/Answer/main.py
+++
@@ -1,6 +0,0 @@
-from pip._vendor.colorama import Fore
-
-print(Fore.WHITE + "Running Naive Bayes-----------------------------------------------")
-import  NaiveBayes
-print(Fore.WHITE + "Running Logistic Regression---------------------------------------")
-import  lr
